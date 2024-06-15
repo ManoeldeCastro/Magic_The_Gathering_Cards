3265621b0750d6b0eee503cd0579182b5ec6b3c5
manoel <manoeldiasmf@gmail.com>
2024-06-15 14:07:16 -0300

    Fix: erro deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+import dotenv from "dotenv"
+
+dotenv.config()
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,4 +12,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    'process.env': process.env
+  }
 })
